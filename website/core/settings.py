910ae3ad58bf884bc204105ff2267e14a6520e50
ayushgupta0010 <ayushgupta0010@gmail.com>
2025-11-11 21:38:42 -0500

    Add models in settings

--- website/core/settings.py
+++ website/core/settings.py
@@ -25,6 +25,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'director',
+    'genre',
+    'movie',
 ]
 
 MIDDLEWARE = [

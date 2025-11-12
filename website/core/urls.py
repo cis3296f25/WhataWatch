916479c60b52a81a07935e5bb236c6faaa9b608d
ayushgupta0010 <ayushgupta0010@gmail.com>
2025-11-11 22:46:47 -0500

    Add views in Movie

--- website/core/urls.py
+++ website/core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('movie.urls')),
     path('admin/', admin.site.urls),
 ]
